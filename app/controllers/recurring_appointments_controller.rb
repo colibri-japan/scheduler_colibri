106e2601ac6e0bea866fff26550855d892e4b874
remidst <remidaste@hotmail.fr>
2018-08-03 13:11:36 +0200

    correctly displaying error message if appointment overlaps for update action

--- app/controllers/recurring_appointments_controller.rb
+++ app/controllers/recurring_appointments_controller.rb
@@ -68,25 +68,34 @@ class RecurringAppointmentsController < ApplicationController
 
     @recurring_appointment.original_id = @original_recurring_appointment.id
 
-    @recurring_appointment.save
-
 
     if params[:appointment].present?
-      @original_recurring_appointment.update(displayable: false)
+    	@recurring_appointment.start = params[:appointment][:start]
+    	@recurring_appointment.end = params[:appointment][:end]
+    	@recurring_appointment.anchor = params[:appointment][:start]
+    	@recurring_appointment.master = false
 
-      @recurring_appointment.update(start: params[:appointment][:start], end: params[:appointment][:end], anchor: params[:appointment][:start], master: false)
+    	if @recurring_appointment.save
+    		@original_recurring_appointment.update(displayable: false)
+    		@activity = @recurring_appointment.create_activity :update, owner: current_user, planning_id: @planning.id, nurse_id: @recurring_appointment.nurse_id, patient_id: @recurring_appointment.patient_id, previous_nurse: @original_recurring_appointment.nurse.name, previous_patient: @original_recurring_appointment.patient.name, previous_start: @original_recurring_appointment.start, previous_end: @original_recurring_appointment.end, previous_anchor: @original_recurring_appointment.anchor
+    	end
+
     else
       if recurring_appointment_params[:master] == '1' && @original_recurring_appointment.master == true
-      	@original_recurring_appointment.update(master: false, displayable: false)
+      	@original_recurring_appointment.master = false
+      	@original_recurring_appointment.displayable = false
       else
-      	@original_recurring_appointment.update(displayable: false)
+      	@original_recurring_appointment.displayable = false
         @recurring_appointment.master = false
       end
 
-      @recurring_appointment.update(recurring_appointment_params)
+      
+      if @recurring_appointment.update(recurring_appointment_params)
+      	@original_recurring_appointment.save
+      	@activity = @recurring_appointment.create_activity :update, owner: current_user, planning_id: @planning.id, nurse_id: @recurring_appointment.nurse_id, patient_id: @recurring_appointment.patient_id, previous_nurse: @original_recurring_appointment.nurse.name, previous_patient: @original_recurring_appointment.patient.name, previous_start: @original_recurring_appointment.start, previous_end: @original_recurring_appointment.end, previous_anchor: @original_recurring_appointment.anchor
+      end
     end  
 
-    @activity = @recurring_appointment.create_activity :update, owner: current_user, planning_id: @planning.id, nurse_id: @recurring_appointment.nurse_id, patient_id: @recurring_appointment.patient_id, previous_nurse: @original_recurring_appointment.nurse.name, previous_patient: @original_recurring_appointment.patient.name, previous_start: @original_recurring_appointment.start, previous_end: @original_recurring_appointment.end, previous_anchor: @original_recurring_appointment.anchor if @recurring_appointment
   end
 
 

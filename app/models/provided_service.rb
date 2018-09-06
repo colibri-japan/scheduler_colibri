d3b2289419816b6146958950644c0c3d782880b5
remidst <remidaste@hotmail.fr>
2018-09-06 23:17:14 +0200

    fix on provided services update script

--- app/models/provided_service.rb
+++ app/models/provided_service.rb
@@ -73,11 +73,13 @@ class ProvidedService < ApplicationRecord
 		provided_services = ProvidedService.where(service_date: nil).where.not(appointment_id: nil)
 
 		provided_services.each do |service|
-			service.service_date = service.appointment.end
-			service.appointment_start = service.appointment.start 
-			service.appointment_end = service.appointment.end
+			if service.appointment.present?
+			  service.service_date = service.appointment.end
+			  service.appointment_start = service.appointment.start 
+			  service.appointment_end = service.appointment.end
 
-			service.save
+			  service.save
+			end
 		end
 	end
 

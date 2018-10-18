8f7296aa4dc4ee8d94e755747b9ed2296bbfacc8
remidst <remidaste@hotmail.fr>
2018-10-18 14:28:09 +0200

    detailed service information in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   before_action :set_corporation
 
+
   def index
     @plannings = @corporation.plannings.where(archived: false).order(created_at: :desc)
     @activities = PublicActivity::Activity.where(planning_id: @plannings.ids, created_at: current_user.last_sign_in_at..current_user.current_sign_in_at)
@@ -8,9 +9,23 @@ class DashboardController < ApplicationController
     if @unseen_activity_count == 0
       @activities = PublicActivity::Activity.where(planning_id: @plannings.ids).last(5)
     end
-    @per_service_count = Appointment.where(planning_id: @plannings.ids, starts_at: Time.now.beginning_of_day..Time.now.end_of_day, master: false, displayable: true, deleted: false, deactivated: false).group('title').count
-    @provided_service_data = ProvidedService.where(planning_id: @plannings.ids, service_date: (Time.now.beginning_of_day - 7.days)..(Time.now.end_of_day), provided: true, temporary: false, deactivated: false).group_by_day(:service_date).sum(:total_wage)
-    @appointments = Appointment.where(planning_id: @plannings.ids, starts_at: Date.today.beginning_of_day..Date.today.end_of_day, master: false, displayable: true, deactivated: false).includes(:patient, :nurse).order(starts_at: :asc).group_by {|appointment| appointment.nurse_id}
+    monthly_appointments = Appointment.where(planning_id: @plannings.ids, starts_at: Date.today.beginning_of_month..Date.today.end_of_day, master: false, displayable: true, deactivated: false).includes(:patient, :nurse)
+    appointments = monthly_appointments.where(starts_at: Date.today.beginning_of_day..Date.today.end_of_day)
+    
+    monthly_provided_services = ProvidedService.where(planning_id: @plannings.ids, service_date: Date.today.beginning_of_month..Date.today, provided: true, temporary: false, deactivated: false).includes(:patient, :nurse)
+    weekly_provided_services = monthly_provided_services.where(service_date: (Date.today - 7.days)..Date.today)
+   
+    @monthly_appointments_grouped_by_title = monthly_appointments.group_by {|e| e.title}
+    @appointments_grouped_by_title = appointments.group_by {|e| e.title}
+    @female_patients_ids = @corporation.patients.where(gender: true).ids 
+    @male_patients_ids = @corporation.patients.where(gender: false).ids
+    @provided_service_data = weekly_provided_services.group_by_day(:service_date).sum(:total_wage)
+    
+    @provided_service_duration_sums = weekly_provided_services.group_by_day(:service_date).sum(:service_duration)
+    @provided_service_duration_sums.each do |service_date, total_duration|
+      total_duration = total_duration / 60
+    end
+    @appointments = appointments.order(starts_at: :asc).group_by {|appointment| appointment.nurse_id}
   end
 
   private

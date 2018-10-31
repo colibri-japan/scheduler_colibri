31275d2b3ef5a14706678cea1d489e7231285434
remidst <remidaste@hotmail.fr>
2018-10-31 01:50:32 +0100

    fix on nurse mailer for cross month delivery

--- app/models/nurse.rb
+++ app/models/nurse.rb
@@ -31,18 +31,20 @@ class Nurse < ApplicationRecord
 
 		@custom_email_days.map! {|e| e.to_date }
 
-		valid_plannings = Planning.where(business_month: Time.current.month, corporation_id: self.corporation_id, archived: false)
+		valid_plannings = Planning.where(business_month: [Time.current.month, Time.current.month + 1], corporation_id: self.corporation_id, archived: false)
 
 		selected_appointments = []
 
 		@custom_email_days.each do |custom_day|
 			custom_day_start = custom_day.beginning_of_day
 			custom_day_end = custom_day.end_of_day
-			custom_day_appointments =  Appointment.valid.where(planning_id: valid_plannings.ids, nurse_id: self.id,edit_requested: false, master: false).where(starts_at: custom_day_start..custom_day_end).all.order(starts_at: 'asc')
+			custom_day_appointments =  Appointment.valid.edit_not_requested.where(planning_id: valid_plannings.ids, nurse_id: self.id, starts_at: custom_day_start..custom_day_end, master: false).all.order(starts_at: 'asc')
 			selected_appointments << custom_day_appointments.to_a
 		end
 
+
 		selected_appointments = selected_appointments.flatten
+
 
 		if selected_appointments.present? && self.phone_mail.present?
 			NurseMailer.reminder_email(self, selected_appointments, @custom_email_days, {custom_email_message: custom_email_message, custom_subject: custom_email_subject}).deliver_now

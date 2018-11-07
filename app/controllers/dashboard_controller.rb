1465097395ce2a374ba1c9ce7eda6de8c8c65cea
remidst <remidaste@hotmail.fr>
2018-11-07 18:10:14 +0100

    showing this week's usage on the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,11 +9,13 @@ class DashboardController < ApplicationController
     if @unseen_activity_count == 0
       @activities = PublicActivity::Activity.where(planning_id: @plannings.ids).last(5)
     end
-    monthly_appointments = Appointment.where(planning_id: @plannings.ids, starts_at: Date.today.beginning_of_month..Date.today.end_of_day, master: false, displayable: true, deactivated: false).includes(:patient, :nurse)
-    appointments = monthly_appointments.where(starts_at: Date.today.beginning_of_day..Date.today.end_of_day)
+    today = Date.today 
+
+    monthly_appointments = Appointment.where(planning_id: @plannings.ids, starts_at: today.beginning_of_month..today.end_of_day, master: false, displayable: true, deactivated: false).includes(:patient, :nurse)
+    appointments = monthly_appointments.where(starts_at: today.beginning_of_day..today.end_of_day)
     
-    monthly_provided_services = ProvidedService.where(planning_id: @plannings.ids, service_date: Date.today.beginning_of_month..Date.today, provided: true, temporary: false, deactivated: false).includes(:patient, :nurse)
-    weekly_provided_services = monthly_provided_services.where(service_date: (Date.today - 7.days)..Date.today)
+    monthly_provided_services = ProvidedService.where(planning_id: @plannings.ids, service_date: today.beginning_of_month..today, provided: true, temporary: false, deactivated: false).includes(:patient, :nurse)
+    weekly_provided_services = monthly_provided_services.where(service_date: (today - 7.days)..today)
    
     @monthly_appointments_grouped_by_title = monthly_appointments.group_by {|e| e.title}
     @appointments_grouped_by_title = appointments.group_by {|e| e.title}
@@ -22,12 +24,14 @@ class DashboardController < ApplicationController
     @provided_service_data = weekly_provided_services.group_by_day(:service_date).sum(:total_wage)
 
     #edit requested appointments for next two weeks
-    today = Date.today.beginning_of_day
-    @edit_requested_appointments = Appointment.where(planning_id: @plannings.ids, starts_at: today..(today + 15.days), edit_requested: true).order(starts_at: :asc)
+    @edit_requested_appointments = Appointment.where(planning_id: @plannings.ids, starts_at: today.beginning_of_day..(today + 15.days).beginning_of_day, edit_requested: true).order(starts_at: :asc)
 
     #appointments with comments for the next two weeks
-    @commented_appointments = Appointment.where(planning_id: @plannings.ids, starts_at: today..(today + 15.days)).where.not(description: [nil, '']).order(starts_at: :asc)
+    @commented_appointments = Appointment.where(planning_id: @plannings.ids, starts_at: today.beginning_of_day..(today + 15.days).beginning_of_day).where.not(description: [nil, '']).order(starts_at: :asc)
     
+    #weekly summary
+    @weekly_appointments_grouped_by_title = monthly_appointments.where(starts_at: (today - (today.strftime('%u').to_i - 1).days).beginning_of_day..today.end_of_day).group_by {|a| a.title}
+
     #posts
     @posts = @corporation.posts.order(created_at: :desc).first(10)
     

fd55d77a68e0290243902fbbef1e50f9f977cd9c
remidst <remidaste@hotmail.fr>
2018-11-21 08:06:50 +0900

    improved efficiency  of query in dashboard index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -15,21 +15,28 @@ class DashboardController < ApplicationController
     #posts
     @posts = @corporation.posts.order(created_at: :desc).first(10)
 
+    #appointments : today, upcoming two weeks, since monday
+    today = Date.today 
+    appointments = Appointment.valid.where(planning_id: @plannings.ids, master: false, starts_at: (today - (today.strftime('%u').to_i - 1).days).beginning_of_day..(today + 15.days).beginning_of_day).includes(:patient, :nurse)
+
     
     #daily summary
-    today = Date.today 
-    @appointments_grouped_by_title = Appointment.where(planning_id: @plannings.ids, master: false, displayable: true, deactivated: false, edit_requested: false).where(starts_at: today.beginning_of_day..today.end_of_day).includes(:patient, :nurse).group_by {|e| e.title}
+    # @appointments_grouped_by_title = Appointment.where(planning_id: @plannings.ids, master: false, displayable: true, deactivated: false, edit_requested: false).where(starts_at: today.beginning_of_day..today.end_of_day).includes(:patient, :nurse).group_by {|e| e.title}
+    @appointments_grouped_by_title = appointments.edit_not_requested.where(starts_at: today.beginning_of_day..today.end_of_day).group_by {|e| e.title}
     @female_patients_ids = @corporation.patients.where(gender: true).ids 
     @male_patients_ids = @corporation.patients.where(gender: false).ids
     
     #weekly summary, from monday to today
-    @weekly_appointments_grouped_by_title = Appointment.where(planning_id: @plannings.ids, master: false, displayable: true, deactivated: false, starts_at: (today - (today.strftime('%u').to_i - 1).days).beginning_of_day..today.end_of_day).includes(:patient, :nurse).group_by {|a| a.title}
+    # @weekly_appointments_grouped_by_title = Appointment.where(planning_id: @plannings.ids, master: false, displayable: true, deactivated: false, starts_at: (today - (today.strftime('%u').to_i - 1).days).beginning_of_day..today.end_of_day).includes(:patient, :nurse).group_by {|a| a.title}
+    @weekly_appointments_grouped_by_title = appointments.edit_not_requested.where(starts_at: (today - (today.strftime('%u').to_i - 1).days).beginning_of_day..today.end_of_day).group_by {|a| a.title}
 
     #edit requested appointments for next two weeks
-    @edit_requested_appointments = Appointment.where(planning_id: @plannings.ids, starts_at: today.beginning_of_day..(today + 15.days).beginning_of_day, edit_requested: true).order(starts_at: :asc)
+    # @edit_requested_appointments = Appointment.where(planning_id: @plannings.ids, starts_at: today.beginning_of_day..(today + 15.days).beginning_of_day, edit_requested: true).order(starts_at: :asc)
+    @edit_requested_appointments = appointments.where(starts_at: today.beginning_of_day..(today + 15.days).beginning_of_day, edit_requested: true).order(starts_at: :asc)
 
     #appointments with comments for the next two weeks
-    @commented_appointments = Appointment.where(planning_id: @plannings.ids, starts_at: today.beginning_of_day..(today + 15.days).beginning_of_day).where.not(description: [nil, '']).order(starts_at: :asc)
+    # @commented_appointments = Appointment.where(planning_id: @plannings.ids, starts_at: today.beginning_of_day..(today + 15.days).beginning_of_day).where.not(description: [nil, '']).order(starts_at: :asc)
+    @commented_appointments = appointments.where(starts_at: today.beginning_of_day..(today + 15.days).beginning_of_day).where.not(description: [nil, '']).order(starts_at: :asc)
 
     #daily provided_services to be verified
     @daily_provided_services = ProvidedService.where(planning_id: @plannings.ids, temporary: false, deactivated: false, service_date: Date.today.beginning_of_day..Date.today.end_of_day).includes(:patient, :nurse).order(service_date: :asc).group_by {|provided_service| provided_service.nurse_id}

9798593bdec6349567edfe9c855c1b29b5fdd707
remidst <remidaste@hotmail.fr>
2018-11-26 18:43:09 +0900

    handy methods for scan model

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -2,4 +2,22 @@ class Scan < ApplicationRecord
   mount_uploader :teikyohyo, TeikyohyoUploader
   
   belongs_to :planning
+
+  def done!
+    self.update(done_at: Time.current)
+  end
+
+  def done?
+    self.done_at.present?
+  end
+
+  def cancel!
+    self.update(cancelled_at: Time.current)
+  end
+
+  def cancelled?
+    self.cancelled_at.present?
+  end
+
+
 end

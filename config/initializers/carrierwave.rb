fad4f9cdb35eece27202c0221d9571e33caacbad
remidst <remidaste@hotmail.fr>
2018-11-26 18:36:33 +0900

    carrierwave aws config

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,19 @@
+require 'carrierwave/orm/activerecord'
+
+CarrierWave.configure do |config|
+  config.ignore_processing_errors = true
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV['S3_KEY'] ,                        # required unless using use_iam_profile
+    aws_secret_access_key: ENV['S3_SECRET'] ,                        # required unless using use_iam_profile
+    region:                ENV['S3_REGION'] ,                  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = ENV['S3_BUCKET']                                    # required
+end
+
+begin
+  # attaching code
+rescue CarrierWave::ProcessingError => error
+  raise error.cause
+end

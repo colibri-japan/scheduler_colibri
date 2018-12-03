e08d22156fd0e40edc8797030c0ca609d50745d7
remidst <remidaste@hotmail.fr>
2018-12-03 11:10:40 +0900

    user role explained in users index view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,5 +8,15 @@ module UsersHelper
 		end
 	end
 
+	def user_role(user)
+		if user.role == 'schedule_restricted'
+			'全体.個別スケジュールのみ'
+		elsif user.role == 'schedule_admin'
+			'スケジュール全権限'
+		elsif user.role == 'corporation_admin'
+			'スケジュール全権限 +　給与詳細'
+		end
+	end
+
 
 end

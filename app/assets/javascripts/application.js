c1a2bc73dac2ac24a3828b89700dbd7e51b9d73b
remidst <remidaste@hotmail.fr>
2018-12-05 15:14:52 +0900

    print dates or not given printing options

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1691,10 +1691,30 @@ $(document).on('turbolinks:load', function(){
 
 
   $('#print-button').click(function(){
+    if (window.printDates === "false") {
+      $('.fc-center').addClass('no-print');
+      $('.fc-day-header.fc-mon').html('月');
+      $('.fc-day-header.fc-tue').html('火');
+      $('.fc-day-header.fc-wed').html('水');
+      $('.fc-day-header.fc-thu').html('木');
+      $('.fc-day-header.fc-fri').html('金');
+      $('.fc-day-header.fc-sat').html('土');
+      $('.fc-day-header.fc-sun').html('日');
+    }
     window.print();    
   });
 
   $('#master-print-button').click(function(){
+    if (window.printDates === "false") {
+      $('.fc-center').addClass('no-print');
+      $('.fc-day-header.fc-mon').html('月');
+      $('.fc-day-header.fc-tue').html('火');
+      $('.fc-day-header.fc-wed').html('水');
+      $('.fc-day-header.fc-thu').html('木');
+      $('.fc-day-header.fc-fri').html('金');
+      $('.fc-day-header.fc-sat').html('土');
+      $('.fc-day-header.fc-sun').html('日');
+    }
     window.print();
   });
 

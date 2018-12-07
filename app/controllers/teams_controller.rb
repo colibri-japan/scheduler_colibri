c970a8509f3fb667618855897764295bbe688cd0
remidst <remidaste@hotmail.fr>
2018-12-07 18:35:18 +0900

    very basic team index create edit

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,46 @@
+class TeamsController < ApplicationController
+
+    before_action :set_corporation
+
+    def index 
+        @teams = @corporation.teams.includes(:nurses)
+    end
+
+    def new 
+        @team = Team.new()
+    end
+
+    def edit
+        @team = Team.find(params[:id])
+    end
+
+    def create
+        @team = @corporation.teams.new(team_params)
+
+        if @team.save 
+            redirect_to teams_path, notice: '新規チームが登録されました'
+        end
+    end
+
+    def update
+        @team = Team.find(params[:id])
+
+        if @team.update(team_params)
+            redirect_to teams_path, notice: 'チーム情報がアップデートされました。'
+        end
+    end
+
+    def delete
+    end
+
+    private
+
+    def team_params
+        params.require(:team).permit(:team_name)
+    end
+
+    def set_corporation
+        @corporation = current_user.corporation 
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,8 @@ Rails.application.routes.draw do
 
   resources :dashboard, only: :index
 
+  resources :teams
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   

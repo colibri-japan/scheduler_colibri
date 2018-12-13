4943f0c8ebb7e546aba14bb218c9cf29eb80ebcf
remidst <remidaste@hotmail.fr>
2018-12-13 15:25:48 +0900

    debugging email made easier

--- app/workers/send_nurse_reminder_worker.rb
+++ app/workers/send_nurse_reminder_worker.rb
@@ -7,24 +7,39 @@ class SendNurseReminderWorker
     custom_email_subject = options['custom_email_subject']
     selected_appointments = []
     @nurse = Nurse.find(nurse_id)
+
+    puts 'nurse fetched by id'
+    puts @nurse.name
     
     custom_email_days.map! {|e| e.to_date }
 
+    puts 'custom email days'
+    puts custom_email_days
+
     valid_plannings = Planning.where(business_month: [Time.current.month, Time.current.month + 1], corporation_id: @nurse.corporation_id, archived: false)
 
+    puts 'valid plannings'
+    puts valid_plannings
+
     if valid_plannings.present?
+      puts 'inside valid plannings presence'
       custom_email_days.each do |date|
         custom_day_start = date.beginning_of_day
         custom_day_end = date.end_of_day
         custom_day_appointments =  Appointment.valid.edit_not_requested.where(planning_id: valid_plannings.ids, nurse_id: nurse_id, starts_at: custom_day_start..custom_day_end, master: false).all.order(starts_at: 'asc')
         selected_appointments << custom_day_appointments.to_a
       end
+
+      puts 'selected appointments'
+      puts selected_appointments
       
       selected_appointments = selected_appointments.flatten
 
       if selected_appointments.present? && @nurse.phone_mail.present?
+        puts 'selected appointments and nurse phone mail presence'
         NurseMailer.reminder_email(@nurse, selected_appointments, custom_email_days, {custom_email_message: custom_email_message, custom_subject: custom_email_subject}).deliver_now
       end
     end
   end
 end
+

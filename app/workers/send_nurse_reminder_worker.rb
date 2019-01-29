f09f4c7b8d573db3819f79f497002560bd6f736a
remidst <remidaste@hotmail.fr>
2019-01-29 21:42:04 +0100

    reminder works for unavailability as well

--- app/workers/send_nurse_reminder_worker.rb
+++ app/workers/send_nurse_reminder_worker.rb
@@ -12,28 +12,15 @@ class SendNurseReminderWorker
 
     planning = @nurse.corporation.planning
 
-    puts 'nurse id'
-    puts nurse_id
-
-    puts 'custom email days'
-    puts custom_email_days
-
-
     custom_email_days.each do |date|
-      puts 'date'
-      puts date 
-      puts date.beginning_of_day
-      puts date.end_of_day
       custom_day_appointments =  Appointment.valid.edit_not_requested.where(planning_id: planning.id, nurse_id: nurse_id, starts_at: date.beginning_of_day..date.end_of_day, master: false).all.order(starts_at: 'asc')
-      puts 'count:'
-      puts custom_day_appointments.count
+      custom_day_unavailabilities = Unavailability.where(planning_id: planning.id, nurse_id: nurse_id, starts_at: date.beginning_of_day..date.end_of_day).order(starts_at: :asc)
       selected_appointments << custom_day_appointments.to_a
+      selected_appointments << custom_day_unavailabilities.to_a
     end
       
     selected_appointments = selected_appointments.flatten
-
-    puts 'selected appointments'
-    puts selected_appointments
+    selected_appointments = selected_appointments.sort_by &:starts_at
 
     if selected_appointments.present? && @nurse.phone_mail.present?
       NurseMailer.reminder_email(@nurse, selected_appointments, custom_email_days, {custom_email_message: custom_email_message, custom_subject: custom_email_subject}).deliver_now

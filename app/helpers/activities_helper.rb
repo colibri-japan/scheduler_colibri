ce5bc7b9b134c3cb85bfe9b049a2fac1928cbc17
remidst <remidaste@hotmail.fr>
2019-02-20 19:07:25 +0100

    refactoring activities dates

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -7,6 +7,8 @@ module ActivitiesHelper
 			'update_32px.png'
 		elsif activity.key.include? "destroy"
 			'destroy_32px.png'
+		elsif activity.key.include? "terminate"
+			'destroy_32px.png'
 		elsif activity.key.include? "toggle_edit_requested"
 			'edit_requested_32px.png'
 		elsif activity.key.include? "archive"

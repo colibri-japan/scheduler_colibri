2266cf8f5f0b77ec443f5040405e6cf336349f67
remidst <remidaste@hotmail.fr>
2019-04-05 19:58:29 +0200

    rails basic form and controller to create salary rule

--- app/controllers/salary_rules_controller.rb
+++ app/controllers/salary_rules_controller.rb
@@ -2,13 +2,34 @@ class SalaryRulesController < ApplicationController
     before_action :set_corporation
 
     def index
-        @salary_rules = @corporation.salary_rules
+      @salary_rules = @corporation.salary_rules
+    end
+
+    def new 
+      set_nurses_and_services
+
+      @salary_rule = @corporation.salary_rules.new
+    end
+
+    def create 
+      @salary_rule = @corporation.salary_rules.new(salary_rules_params)
+
+      @salary_rule.save
     end
 
     private
 
     def set_corporation
       @corporation = Corporation.cached_find(current_user.corporation_id)
+    end
+
+    def set_nurses_and_services
+      @nurses = @corporation.nurses.displayable
+      @services = @corporation.services.where(nurse_id: nil)
+    end
+
+    def salary_rules_params
+      params.require(:salary_rule).permit(:title, :target_all_nurses, :target_all_services, :date_constraint, :expires_at, :operator, :argument, nurse_id_list: [], service_title_list: [])
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,7 @@ Rails.application.routes.draw do
 
   resources :contact_forms, only: [:new, :create]
 
-  resources :salary_rules, only: [:index]
+  resources :salary_rules, only: [:index, :new, :create]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

e30f01245964262bad1b1c644beaa5c082f594c9
remidst <remidaste@hotmail.fr>
2019-05-02 17:33:07 +0200

    saving first steps of teikyohyo

--- config/routes.rb
+++ config/routes.rb
@@ -100,13 +100,14 @@ Rails.application.routes.draw do
   patch 'nurses/:id/send_reminder_email' => 'nurses#send_reminder_email', as: :nurse_send_reminder_email
   patch 'nurses/:id/master_to_schedule' => 'nurses#master_to_schedule', as: :nurse_master_to_schedule
   get 'nurses/:id/new_master_to_schedule' => "nurses#new_master_to_schedule", as: :new_nurse_master_to_schedule
-
+  
   #custom routes for patients
   get 'plannings/:planning_id/patients/:id/master' => 'patients#master', as: :planning_patient_master
   patch 'patients/:id/toggle_active' => 'patients#toggle_active', as: :toggle_active
   patch 'patients/:id/master_to_schedule' => 'patients#master_to_schedule', as: :patient_master_to_schedule
   get 'patients/:id/new_master_to_schedule' => "patients#new_master_to_schedule", as: :new_patient_master_to_schedule
   get 'plannings/:planning_id/patients/:id/payable' => 'patients#payable', as: :planning_patient_payable
+  get 'patients/:id/teikyohyo' => 'patients#teikyohyo', as: :patient_teikyohyo
 
   #custom routes for plannings
   get 'plannings/:id/monthly_general_report' => 'plannings#monthly_general_report', as: :planning_monthly_general_report

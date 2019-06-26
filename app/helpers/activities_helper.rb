e4d1200cfb6178400dd699199e17ffeaefebcda8
remidst <remidaste@hotmail.fr>
2019-06-26 23:24:35 +0200

    using svg as activity icons

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -2,31 +2,31 @@ module ActivitiesHelper
 
 	def activity_icon(activity_key)
 		if activity_key.include? "create"
-			'create_32px.png'
+			'create.svg'
 		elsif activity_key.include? "update"
-			'update_32px.png'
+			'edit_icon.svg'
 		elsif activity_key.include? "destroy"
-			'destroy_32px.png'
+			'delete_icon.svg'
 		elsif activity_key.include? "terminate"
-			'destroy_32px.png'
+			'delete_icon.svg'
 		elsif activity_key.include? "toggle_edit_requested"
-			'edit_requested_32px.png'
+			'edit_requested_icon.svg'
 		elsif activity_key.include? "archive"
-			'destroy_32px.png'
+			'delete_icon.svg'
 		elsif activity_key.include? "toggle_cancelled"
-			'cancel_32px.png'
+			'cancel.svg'
 		elsif activity_key.include? "batch_request_edit"
-			'edit_requested_32px.png'
+			'edit_requested_icon.svg'
 		elsif activity_key.include? "batch_cancel"
-			'cancel_32px.png'
+			'cancel.svg'
 		elsif activity_key.include? "batch_archive"
-			'destroy_32px.png'
+			'delete_icon.svg'
 		elsif activity_key.include? "reflect_all_master"
-			'duplicate_32px.png'
+			'duplicate_icon.svg'
 		elsif activity_key.include? "reflect_nurse_master"
-			'duplicate_32px.png'
+			'duplicate_icon.svg'
 		elsif activity_key.include? "reflect_patient_master"
-			'duplicate_32px.png'
+			'duplicate_icon.svg'
 		else
 			' '
 		end

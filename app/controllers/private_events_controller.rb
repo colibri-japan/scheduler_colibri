c901107b7780bf352d1704075a1f60140e75182f
remidst <remidaste@hotmail.fr>
2019-06-27 23:35:24 +0200

    private events activities update

--- app/controllers/private_events_controller.rb
+++ app/controllers/private_events_controller.rb
@@ -33,6 +33,7 @@ class PrivateEventsController < ApplicationController
 	end
 
 	def edit
+		@activities = PublicActivity::Activity.where(trackable_type: 'PrivateEvent', trackable_id: @private_event.id, planning_id: @planning.id).includes(:owner)
 	end
 
 	def create
@@ -41,7 +42,7 @@ class PrivateEventsController < ApplicationController
 		
 	  	respond_to do |format|
 	    	if @private_event.save
-	    	  @activity = @private_event.create_activity :create, owner: current_user, planning_id: @planning.id, patient_id: @private_event.patient_id
+	    	  @activity = @private_event.create_activity :create, owner: current_user, planning_id: @planning.id, patient_id: @private_event.patient_id, nurse_id: @private_event.nurse_id, parameters: {nurse_name: @private_event.nurse.try(:name), patient_name: @private_event.nurse.try(:name), starts_at: @private_event.starts_at, ends_at: @private_event.ends_at, title: @private_event.title}
 	    	  format.js
 	    	else
 	    	  format.js
@@ -53,12 +54,12 @@ class PrivateEventsController < ApplicationController
 		authorize @planning, :same_corporation_as_current_user?
 		
 	  	respond_to do |format|
-	    	if @private_event.update(private_event_params)
-	    	  @activity = @private_event.create_activity :update, owner: current_user, planning_id: @planning.id, patient_id: @private_event.patient_id
+			if @private_event.update(private_event_params)
+				create_activity_for_update
 
-	    	  format.js
+	    	    format.js
 	    	else
-	    	  format.js
+	    	    format.js
 	    	end
 	  	end
 	end
@@ -66,7 +67,7 @@ class PrivateEventsController < ApplicationController
 	def destroy
 	  authorize @planning, :same_corporation_as_current_user?
 
-	  @activity = @private_event.create_activity :destroy, owner: current_user, planning_id: @planning.id, patient_id: @private_event.patient_id
+	  @activity = @private_event.create_activity :destroy, owner: current_user, planning_id: @planning.id, patient_id: @private_event.patient_id, nurse_id: @private_event.nurse_id, parameters: {nurse_name: @private_event.nurse.try(:name), patient_name: @private_event.patient.try(:name), starts_at: @private_event.starts_at, ends_at: @private_event.ends_at, title: @private_event.title}
 
 	  @private_event.destroy
 	  respond_to do |format|
@@ -76,8 +77,24 @@ class PrivateEventsController < ApplicationController
 
 	private
 
+	def create_activity_for_update
+		parameters = @private_event.saved_changes
+		parameters["title"] = [@private_event.title, nil] unless parameters["title"].present?
+		parameters["starts_at"] = [@private_event.starts_at, nil] unless parameters["starts_at"].present?
+		parameters["ends_at"] = [@private_event.ends_at, nil] unless parameters["ends_at"].present?
+		parameters["previous_nurse_name"] = Nurse.find(parameters["nurse_id"][0]) if parameters["nurse_id"].present?
+		parameters["previous_patient_name"] = Nurse.find(parameters["patient_id"][0]) if parameters["patient_id"].present?
+		parameters["nurse_name"] = @private_event.nurse.try(:name)
+		parameters["patient_name"] = @private_event.patient.try(:name)
+		previous_nurse_id = parameters["nurse_id"].present? ? parameters["nurse_id"][0] : nil
+        previous_patient_id = parameters["patient_id"].present? ? parameters["patient_id"][0] : nil
+		parameters.delete("updated_at")
+
+		@activity = @private_event.create_activity :update, owner: current_user, planning_id: @planning.id, patient_id: @private_event.patient_id, nurse_id: @private_event.nurse_id, parameters: parameters
+	end
+
 	def set_private_event
-	  @private_event = PrivateEvent.find(params[:id])
+	    @private_event = PrivateEvent.find(params[:id])
 	end
 
 	def set_planning

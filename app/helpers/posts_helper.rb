b0ff349845caabbc437f32a88ad7054d36b241fd
remidst <remidaste@hotmail.fr>
2019-07-30 22:36:49 +0900

    hiding posts with too many patients

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,4 +3,12 @@ module PostsHelper
     def post_was_published_at(post)
         post.published_at.strftime("%Y年%-m月%-d日 %H:%M")
     end
+
+    def post_patients_names(patient_names_array)
+        if patient_names_array.size <= 3
+            " > #{patient_names_array.join('、')}"
+        else
+            " > #{patient_names_array[0..2].join('、')} (その他#{patient_names_array.size - 3}人)"
+        end
+    end
 end

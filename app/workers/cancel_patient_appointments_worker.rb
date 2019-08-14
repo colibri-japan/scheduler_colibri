6e2d129125d3b3746c637f391fbf8a254ad17e1a
remidst <remidaste@hotmail.fr>
2019-08-14 12:23:21 +0900

    small fixes on patient deletion

--- app/workers/cancel_patient_appointments_worker.rb
+++ app/workers/cancel_patient_appointments_worker.rb
@@ -4,14 +4,10 @@ class CancelPatientAppointmentsWorker
 
   def perform(patient_id)
     patient = Patient.find(patient_id)
-    puts patient.name
-
-    planning = patient.corporation.planning
 
     now = Time.current.in_time_zone('Tokyo')
 
-	  Appointment.not_archived.where(patient_id: patient_id).where('starts_at > ?', now.beginning_of_day).update_all(cancelled: true, updated_at: now)
-	  SalaryLineItem.where('service_date > ?', now).where(patient_id: patient_id).update_all(cancelled: true, total_wage: 0, total_credits: 0, invoiced_total: 0, updated_at: now)
-	  RecurringAppointment.not_archived.where(planning_id: planning.id, patient_id: patient_id).where('(recurring_appointments.termination_date IS NULL) OR ( recurring_appointments.termination_date > ?)', now).update_all(termination_date: now, updated_at: now)
+	  patient.appointments.not_archived.where('starts_at > ?', now.beginning_of_day).update_all(cancelled: true, updated_at: now)
+	  patient.recurring_appointments.not_archived.where('(recurring_appointments.termination_date IS NULL) OR ( recurring_appointments.termination_date > ?)', now).update_all(termination_date: now, updated_at: now)
   end
 end

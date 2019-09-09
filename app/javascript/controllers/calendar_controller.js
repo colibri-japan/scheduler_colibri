e5d57fce978b7f7557ff043a01512829fca784e4
remidst <remidaste@hotmail.fr>
2019-09-09 23:09:21 +0200

    basic navigation for nurse and patient

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -4,28 +4,48 @@ import { Calendar } from '@fullcalendar/core'
 import interactionPlugin from '@fullcalendar/interaction'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
-import listPlugin from '@fullcalendar/list'
+import bootstrapPlugin from '@fullcalendar/bootstrap'
+
 
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
 import '@fullcalendar/timegrid/main.css';
 import '@fullcalendar/list/main.css';
 
+let calendarEl = document.getElementById('nurse-calendar')
 
+let calendar = new Calendar(calendarEl, {
+    plugins: [interactionPlugin, timeGridPlugin, dayGridPlugin, bootstrapPlugin],
+    schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+    firstDay: window.firstDay,
+    themeSystem: 'unthemed',
+    slotDuration: '00:15:00',
+    timeFormat: 'H:mm',
+    nowIndicator: true,
+    defaultView: 'timeGridWeek',
+    locale: 'ja',
+    header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'timeGridDay,timeGridWeek,dayGridMonth'
+    },
+    minTime: window.minTime,
+    maxTime: window.maxTime,
+
+    events: '/plannings/62/appointments?nurse_id=39'
+})
 
 export default class extends Controller {
 
     static targets = [ 'resourceName' ]
     
     connect() {
-        console.log('connected')
-        this.getCalendar().render()
+        calendar.render()
         this.initializeResource()
     }
 
     disconnect() {
-        console.log('disconnected')
-        this.getCalendar().destroy()
+        calendar.destroy()
     }
 
     navigate(event) {
@@ -34,18 +54,20 @@ export default class extends Controller {
             el.classList.remove('resource-selected')
         }
         event.target.classList.add('resource-selected')
+
+        window.currentResourceType = event.target.dataset.resourceType
+        window.currentResourceId = event.target.dataset.resourceId
+
+        let eventSources = calendar.getEventSources()
+
+        for (let source of eventSources) {
+            source.remove()
+        }
+        
+        calendar.addEventSource(`${window.appointmentsUrl}?${window.currentResourceType}_id=${window.currentResourceId}`)
+        calendar.refetchEvents()
+
         this.resourceNameTarget.textContent = event.target.textContent
-    }
-
-    getCalendar() {
-        let calendarEl = document.getElementById('nurse-calendar')
-
-        let calendar = new Calendar(calendarEl, {
-            plugins: [interactionPlugin, timeGridPlugin],
-            defaultView: 'timeGridWeek'
-        })
-
-        return calendar
     }
 
     initializeResource() {
@@ -54,6 +76,8 @@ export default class extends Controller {
 
         let selectedItem = document.getElementById(`${resourceType}_${resourceId}`)
         selectedItem.classList.add('resource-selected')
+
+        this.resourceNameTarget.textContent = selectedItem.textContent
 
         return
     }

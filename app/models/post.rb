c2ae6f0e392b6d9baf9f4d00788ebf9d8f028d62
remidst <remidaste@hotmail.fr>
2019-11-08 09:05:55 +0100

    fix on posts showing the wrong day

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,7 +21,7 @@ class Post < ApplicationRecord
 
   def self.mark_reminderable_as_unread
     start_time = (Time.current + 1.day).beginning_of_day
-    reminderable_ids = Reminder.where(reminderable_type: 'Post').occurs_in_range(start_time..(start_time + 1.day)).pluck(:reminderable_id)
+    reminderable_ids = Reminder.where(reminderable_type: 'Post').occurs_in_range(start_time..(start_time.end_of_day)).pluck(:reminderable_id)
     Post.where(id: reminderable_ids).update_all(updated_at: Time.current)
   end
 

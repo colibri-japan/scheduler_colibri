754dab5f3f55eecdb463bb5f6bfe9fcbffe3bafe
remidst <remidaste@hotmail.fr>
2019-11-13 12:57:35 +0900

    vertical menu for smartphone portrait

--- app/javascript/application/javascripts/general_javascript.js
+++ app/javascript/application/javascripts/general_javascript.js
@@ -3,4 +3,20 @@ document.addEventListener('turbolinks:load', function () {
         var aTag = $("#" + $(this).data('anchor'));
         $('html,body').animate({ scrollTop: aTag.offset().top }, 'slow')
     })
+
+    $('#hamburger-nav-mobile').click(function(){
+        $(this).hide()
+        $('#close-nav-mobile').show()
+        $('#colibri-mobile-menu').show()
+    })
+
+    $('#close-nav-mobile').click(function(){
+        $(this).hide()
+        $('#hamburger-nav-mobile').show()
+        $('#colibri-mobile-menu').hide()
+    })
+
+    $('.toggle-submenu').click(function(){
+        $(this).next().toggle()
+    })
 })

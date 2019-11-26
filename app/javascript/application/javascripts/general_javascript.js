e1f7d3295d7810e43ec814cb9068748252fc41ea
remidst <remidaste@hotmail.fr>
2019-11-26 12:23:30 +0900

    created a custom dropdown menu

--- app/javascript/application/javascripts/general_javascript.js
+++ app/javascript/application/javascripts/general_javascript.js
@@ -20,4 +20,23 @@ document.addEventListener('turbolinks:load', function () {
         $(this).next().toggle()
     })
 
+    window.colibriDropdown = function() {
+        document.getElementById("colibriDropdown").classList.toggle('show')
+    }
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("colibri-dropdown-content")
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('dropdown-show')) {
+                    openDropdown.classList.remove('dropdown-show');
+                }
+            }
+        }
+    }
+
+
+
 })

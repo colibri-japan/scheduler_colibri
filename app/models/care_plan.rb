4af0c61f941add59ddf779dc2177fe9a2d12c395
remidst <remidaste@hotmail.fr>
2020-03-22 10:19:49 +0900

    make the care manager optional in patient form

--- app/models/care_plan.rb
+++ app/models/care_plan.rb
@@ -1,5 +1,5 @@
 class CarePlan < ApplicationRecord
-    belongs_to :care_manager
+    belongs_to :care_manager, optional: true
     belongs_to :patient
 
     scope :valid_at, -> date { where('kaigo_certification_validity_start <= ? AND kaigo_certification_validity_end >= ?', date, date) }

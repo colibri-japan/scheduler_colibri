7a997145bbd63b6549c5b9b7d2a82779b66cdfd9
remidst <remidaste@hotmail.fr>
2020-04-19 11:38:38 +0900

    fix on google analytics and turbolinks

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,10 +1,5 @@
 document.addEventListener('turbolinks:load', function () {
     if (typeof gtag === 'function') {
-        console.log('type of gtag is function')
         gtag('config', 'UA-136584111-3')
-    } else {
-        console.log('gtag is not a function')
     }
-    console.log('firing config anyways')
-    gtag('config', 'UA-136584111-3')
 });

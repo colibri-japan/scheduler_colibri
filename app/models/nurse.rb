2966c4e198e3c1c99f7067060dbee6127bd23aa5
remidst <remidaste@hotmail.fr>
2020-05-05 11:33:52 +0900

    fix on nurse salary pdf total days worked

--- app/models/nurse.rb
+++ app/models/nurse.rb
@@ -149,12 +149,13 @@ class Nurse < ApplicationRecord
 	def self.payable_summary_for(range)
 		return_hash = {}
 
+		#N+1 query that should be tackled
 		Nurse.where(id: self.ids).order_by_kana.each do |nurse|
 			nurse_data = {}
 			appointments_data = nurse.appointments.in_range(range).edit_not_requested.not_archived.order(:starts_at)
 			salary_line_items_data = nurse.salary_line_items.not_from_appointments.in_range(range).order(:service_date, :title)
 
-			return_hash[nurse] = {appointments: appointments_data, salary_line_items: salary_line_items_data, total_days_worked: appointments_data.pluck(:starts_at).map {|e| e.to_date}.uniq.size, total_time_worked: appointments_data.sum(:duration)} 
+			return_hash[nurse] = {appointments: appointments_data, salary_line_items: salary_line_items_data, total_days_worked: appointments_data.not_cancelled.pluck(:starts_at).map {|e| e.to_date}.uniq.size, total_time_worked: appointments_data.not_cancelled.sum(:duration)} 
 		end
 
 		return_hash

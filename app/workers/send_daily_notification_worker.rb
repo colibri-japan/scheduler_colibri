8c51664b5bf33fb01961902032b3e8573ce046a2
remidst <remidaste@hotmail.fr>
2020-07-24 16:31:16 +0200

    fix on sending notification individually

--- app/workers/send_daily_notification_worker.rb
+++ app/workers/send_daily_notification_worker.rb
@@ -6,27 +6,24 @@ class SendDailyNotificationWorker
         date = date_time.to_date rescue nil
         return if date.nil?
 
-        android_registration_ids = []
-        ios_registration_ids = []
-
         Nurse.with_operational_appointments_between(date.beginning_of_day, date.end_of_day).includes(:user).find_each do |nurse|
             user = nurse.user
             if user.present? && user.with_mobile_device?
-                android_registration_ids << user.android_fcm_token if user.android_fcm_token.present?
-                ios_registration_ids << user.ios_fcm_token if user.ios_fcm_token.present?
+                save_android_notification([user.android_fcm_token]) if user.android_fcm_token.present?
+                save_ios_notification([user.ios_fcm_token]) if user.ios_fcm_token.present?
             end
         end
 
-        android_registration_ids = android_registration_ids.flatten
-        ios_registration_ids = ios_registration_ids.flatten
+        #make sure that all notifications are properly saved in the db
+        sleep(10)
 
-        send_android_notification(android_registration_ids) if android_registration_ids.present?
-        send_ios_notification(ios_registration_ids) if ios_registration_ids.present?
+        #push the batch of notifications with Rpush
+        Rpush.push
     end
 
     private 
 
-    def send_android_notification(registration_ids)
+    def save_android_notification(registration_ids)
         app = Rpush::Gcm::App.find_by_name("Colibri")
         n = Rpush::Gcm::Notification.new 
         n.app = app 
@@ -35,13 +32,11 @@ class SendDailyNotificationWorker
         n.priority = 'high'
         if n.save!
             n.data = {title: 'おはようございます！', body: '本日のサービスのまとめです', url: "https://scheduler.colibri.jp/users/current_user_home?to=planning&force_list_view=true&notification_id=#{n.id}"}
-            if n.save!
-                Rpush.push
-            end
+            n.save!
         end
     end
 
-    def send_ios_notification(registration_ids)
+    def save_ios_notification(registration_ids)
         app = Rpush::Gcm::App.find_by_name("Colibri")
         n = Rpush::Gcm::Notification.new 
         n.app = app 
@@ -52,9 +47,7 @@ class SendDailyNotificationWorker
         
         if n.save!
             n.data = {url: "https://scheduler.colibri.jp/users/current_user_home?to=planning&force_list_view=true&notification_id=#{n.id}"}
-            if n.save! 
-                Rpush.push 
-            end
+            n.save! 
         end
     end
 

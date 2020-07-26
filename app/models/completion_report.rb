fc20b85f000e03b89ec112ef0a052b8134449c65
remidst <remidaste@hotmail.fr>
2020-07-26 17:42:39 +0200

    save only one report per appointment

--- app/models/completion_report.rb
+++ app/models/completion_report.rb
@@ -7,6 +7,8 @@ class CompletionReport < ApplicationRecord
     belongs_to :forecasted_report, class_name: 'CompletionReport', optional: true 
     has_many :achieved_reports, class_name: 'CompletionReport', foreign_key: :forecasted_report_id
 
+    validates :reportable_id, uniqueness: {scope: :reportable_type, message: 'はサービスごとに一回しか記録できません'}
+
     before_create :add_reference_to_planning_and_patient
     before_update :split_recurring_appointments_and_attach_completion_reports
 
--- test/models/completion_report_test.rb
+++ test/models/completion_report_test.rb
@@ -4,4 +4,10 @@ class CompletionReportTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save report to appointment with existing report" do
+    report = CompletionReport.new(reportable_type: 'Appointment', reportable_id: 1, assisted_patient_to_eat: true, planning_id: 1, patient_id: 1)
+
+    assert_not report.save, 'Saved a completion report on top of existing report'
+  end
 end
